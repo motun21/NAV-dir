7072e21632e359bf21a01d6d7cb3ade2859bf991
motun21 <motun21@tamu.edu>
2024-02-13 12:44:18 -0600

    Changes to code
    Change the name of website to NAV APP
    Fixed some code errors in main.js

--- my-vue-app/main.js
+++ my-vue-app/main.js
@@ -1,15 +1,15 @@
 import './style.css'
 
-import * as THREE from "three";
+import * as THREE from 'three';
 
-const scene = new THREE.scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth, window.innerHeight, 0.1, 1000);
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGL1Renderer({
-    canvas: document.querySelector('#bg')
+  canvas: document.querySelector('#bg'),
 });
 
 renderer.setPixelRatio( window.devicePixelRatio);
-renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.setZ(30);
+renderer.setSize(window.innerWidth, window.innerHeight); //sets size of the rendering window
+camera.position.setZ(30); //sets the position of the camera moved along the z axis
 
-renderer.render(scene , camera)
+renderer.render(scene , camera); //This draws the scene
