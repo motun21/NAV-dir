73ac6e02a0ee2ce4455d91eaf27fe504a0c86161
motun21 <motun21@tamu.edu>
2024-02-13 13:10:13 -0600

    Added the weird angel shape

--- my-vue-app/main.js
+++ my-vue-app/main.js
@@ -13,3 +13,18 @@ renderer.setSize(window.innerWidth, window.innerHeight); //sets size of the rend
 camera.position.setZ(30); //sets the position of the camera moved along the z axis
 
 renderer.render(scene , camera); //This draws the scene
+
+const geometry = new THREE.TorusKnotGeometry( 10, 3, 100, 16 );  //defines the geometry of the thingy
+const material = new THREE.MeshBasicMaterial( { color: 0xFF6347, wireframe: true } ); //defines the materail of the thingy
+const angel = new THREE.Mesh( geometry, material ); // combines those two things to make a real thing
+
+scene.add(angel)
+
+function animate(){
+  requestAnimationFrame(animate);
+  angel.rotation.x += 0.01;
+  angel.rotation.z += 0.005;
+  renderer.render(scene , camera);
+}
+
+animate()
