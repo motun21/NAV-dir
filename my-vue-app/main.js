4a20c5d3938484a38120b072502ec04dee6b9fa2
motun21 <motun21@tamu.edu>
2024-02-14 09:28:51 -0600

    Update main.js

--- my-vue-app/main.js
+++ my-vue-app/main.js
@@ -10,7 +10,12 @@ const renderer = new THREE.WebGL1Renderer({
 
 renderer.setPixelRatio( window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight); //sets size of the rendering window
-camera.position.setZ(30); //sets the position of the camera moved along the z axis
+camera.position.setZ(40);
+
+/*
+camera.position.setZ(50); //sets the position of the camera moved along the z axis
+camera.position.setY(15);
+*/
 
 renderer.render(scene , camera); //This draws the scene
 
@@ -20,10 +25,21 @@ const angel = new THREE.Mesh( geometry, material ); // combines those two things
 
 scene.add(angel)
 
+const geo = new THREE.PlaneGeometry(25,25);
+const floor = new THREE.Mesh( geo, material );
+
+scene.add(floor) //floor that the builldings are going to be placed on
+floor.position.setY(-20);
+floor.rotation.x = 180
+
+//const controls = new OrbitControls(camera, renderer.domElement); For some reason the orbit camera eliminates all objects in scene, need to fix this bug
+
 function animate(){
   requestAnimationFrame(animate);
   angel.rotation.x += 0.01;
   angel.rotation.z += 0.005;
+
+  //controls.update(); 
   renderer.render(scene , camera);
 }
 
