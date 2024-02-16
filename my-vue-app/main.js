ce5343b38b604f1d7743798c6de60109f7c57337
motun21 <motun21@tamu.edu>
2024-02-16 08:25:51 -0600

    Update main.js

--- my-vue-app/main.js
+++ my-vue-app/main.js
@@ -12,11 +12,7 @@ const renderer = new THREE.WebGL1Renderer({
 renderer.setPixelRatio( window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight); //sets size of the rendering window
 camera.position.setZ(40);
-
-/*
-camera.position.setZ(50); //sets the position of the camera moved along the z axis
-camera.position.setY(15);
-*/
+const controls = new OrbitControls(camera, renderer.domElement); //adds the orbit controls I really dont know how this works but yolo
 
 renderer.render(scene , camera); //This draws the scene
 
@@ -24,26 +20,25 @@ const geometry = new THREE.TorusKnotGeometry( 10, 3, 100, 16 );  //defines the g
 const material = new THREE.MeshBasicMaterial( { color: 0xFF6347, wireframe: true } ); //defines the materail of the thingy
 const angel = new THREE.Mesh( geometry, material ); // combines those two things to make a real thing
 
-scene.add(angel)
 
-const controls = new OrbitControls(camera, renderer.domElement); //adds the orbit controls I really dont know how this works but yolo
 
-const geo = new THREE.PlaneGeometry(25,25);
+
+const geo = new THREE.PlaneGeometry(50,25);
 const floor = new THREE.Mesh( geo, material );
+//floor that the builldings are going to be placed on
+floor.position.setY(0);
+floor.rotation.x = 99
 
-scene.add(floor) //floor that the builldings are going to be placed on
-floor.position.setY(-20);
-floor.rotation.x = 180
+const gridHelper = new THREE.GridHelper(200, 50);
+const hlp = new THREE.AxesHelper(100);
 
-//const controls = new OrbitControls(camera, renderer.domElement); For some reason the orbit camera eliminates all objects in scene, need to fix this bug
-
+scene.add( floor ,gridHelper, hlp)
 function animate(){
   requestAnimationFrame(animate);
   angel.rotation.x += 0.01;
   angel.rotation.z += 0.005;
 
   controls.update();
-  //controls.update(); 
   renderer.render(scene , camera);
 }
 
