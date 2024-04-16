7a96da526ed2adabb35effbbe27c2d02642bf250
bandarim <mb77@tamu.edu>
2024-04-16 11:18:21 -0500

    main update

--- nick_development/main.js
+++ nick_development/main.js
@@ -4,6 +4,8 @@ import { FontLoader } from 'three/addons/loaders/FontLoader.js';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
 import {TextGeometry} from 'three/addons/geometries/TextGeometry.js';
 
+
+
 //scene
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x000000);
@@ -36,8 +38,8 @@ scene.add(plane);
 scene.add(new THREE.AmbientLight(0xffffff, 1));
 
 //helpers
-scene.add(new THREE.GridHelper(500,500));
-scene.add(new THREE.AxesHelper(100));//orange is x axis, green is y, and blue is z
+scene.add(new THREE.GridHelper(200,200));
+scene.add(new THREE.AxesHelper(15));
 
 //word making
 const canvas = document.createElement('canvas');
@@ -55,131 +57,55 @@ material.transparent = true;
 var mesh = new THREE.Mesh(new THREE.PlaneGeometry(100,100), material);
 scene.add(mesh);
 
+//background
+
+const wallpaperLoader = new THREE.TextureLoader();
+const bgTexture = wallpaperLoader.load('imgs\space.jpg');
+// bgTexture.colorSpace = THREE.SRGBColorSpace;
+scene.background = '0xffffff';
+
+//arrow
+
 //words
 const loader = new FontLoader();
 loader.load('src/fonts/Work_Sans_Medium_Regular.json', function (font){
   const geo = new TextGeometry('THIS WORKS', {
     font: font,
-    size: 5,
+    size: 50,
     height: 20
   });
   const textMesh = new THREE.Mesh(geo, [
     new THREE.MeshBasicMaterial(0xffffff)
   ]);
+  // textMesh.position.x = 0;
+  // textMesh.position.y = 0;
+  // textMesh.position.z = 0;
+ 
+  // textMesh.rotation.x = 0;
+  // textMesh.rotation.y = Math.PI * 2;
   scene.add(textMesh);
 });
 
-
-// loader.load('src/fonts/Work_Sans_Medium_Regular.json' , function (font){
-//   const gem = new TextGeometry('This is not Rad', {
-//     font: font,
-//     size: 40,
-//     height: 40,
-//     curveSegments: 12,
-//     bevelEnabled: true,
-//     bevelThickness: 10,
-//     bevelSize: 8,
-//     bevelOffset: 0,
-//     bevelSegments: 5
-//   })
- 
-//   const textMesh1 = new THREE.Mesh(gem, [
-//     new THREE.MeshPhongMaterial({ color: 0x89CFEF }), //front
-//     new THREE.MeshPhongMaterial({color: 0x57A0D2}) //side
-//   ])
-//   console.log(textMesh1)
-//   textMesh1.position.x = 0;
-//   textMesh1.position.y = 0;
-//   textMesh1.position.z = 0;
- 
-//   textMesh1.rotation.x = 0;
-//   textMesh1.rotation.y = Math.PI * 2;
- 
-//   scene.add(textMesh1);
-// });
-
-//background
-const wallpaperLoader = new THREE.TextureLoader();
-const bgTexture = wallpaperLoader.load('imgs\space.jpg');
-// bgTexture.colorSpace = THREE.SRGBColorSpace;
-scene.background = '0xffffff';
-export const global_array = []
-//Tall Building Function
-export function tall_builds(xpos = 0 , zpos = 0, ypos = 21){
-  const geom = new THREE.BoxGeometry( 10, 40, 10 );
-  const mat = new THREE.MeshBasicMaterial( { color: 0xFF6347, wireframe: true } );
-  const tall_build = new THREE.Mesh( geom, mat );
-  tall_build.position.set(xpos, ypos, zpos);
-  scene.add(tall_build);
-}
-
-//Small Building Function
-export function small_buildings(xpos = 0 , zpos = 0, ypos = 11){
-  const geom = new THREE.BoxGeometry( 10, 20, 10 );
-  const mat = new THREE.MeshBasicMaterial( { color: 0xFF6347, wireframe: true } );
-  const small_buildings = new THREE.Mesh( geom, mat );
-  small_buildings.position.set(xpos, ypos, zpos);
-  scene.add(small_buildings);
-}
-
-//Reed Stadium
-export function area_builds(xpos = 0 , zpos = 0, ypos = 1 ){
-  const geom = new THREE.DodecahedronGeometry(40 ,0);
-  const mat = new THREE.MeshBasicMaterial( { color: 0xFF6347, wireframe: true } );
-  const area_buildings = new THREE.Mesh( geom, mat );
-  area_buildings.position.set(xpos, ypos, zpos);
-  scene.add(area_buildings);
-}
-
-//Words Functions
-export function build_words(xpos = 0 , zpos = 0, sentence = "Nothing placed", ypos = 60){
-  loader.load('src/fonts/Work_Sans_Medium_Regular.json', function (font){
-    const geo = new TextGeometry(sentence, {
-      font: font,
-      size: 5,
-      height: 20
-    });
-    const textMesh = new THREE.Mesh(geo, [
-      new THREE.MeshBasicMaterial(0xffffff)
-    ]);
-    textMesh.position.set(xpos, ypos, zpos)
-    global_array.push(textMesh);
-    scene.add(textMesh);
-  });
-}
-
-
-///area 1
-tall_builds(-150,100);
-tall_builds(-170,80);
-tall_builds(-140,80);
-small_buildings(-150,60);
-build_words(-150,70,"area1")
-
-//read area 1
-area_builds(60, -60);
-build_words(45,-60,"reed arena")
-
-//area 3
-small_buildings(150,65)
-small_buildings(170,50)
-small_buildings(155,50)
-tall_builds(170 ,65 )
-small_buildings(180,30)
-build_words(160,55,"area2")
-
-//keybaord listener and hopefully arrow stuff
-document.addEventListener("keydown", onDocumentKeyDown, false);
-
-
+// const fontLoader = new FontLoader();
+// fontLoader.load("node_modules/three/exapmles/fonts/droid/droid_seriff_regular.typeface.json",
+//   (droidFont) => {
+//     const tgem = new THREE.TextGeometry('This is not Rad', {
+//       font: droidFont,
+//       size: 6,
+//       height: 2
+//     });
+//     const textMaterial = new THREE.MeshBasicMaterial();
+//     const textMesh = new THREE.Mesh(tgem, textMaterial);
+//     textMesh.position.x = 100
+//     textMesh.position.x = 100
+//     scene.add(textMesh);
+//   }
+// );
 
 
 function animate() {
   renderer.render(scene, camera);
   requestAnimationFrame(animate);
-  for (let i = 0; i < global_array.length; i++) {
-    global_array[i].quaternion.copy(camera.quaternion);
-  }
-  }
+}
 
-animate()
+animate();
